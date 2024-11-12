e25989909c49e0aacbccd27d5d23b3af8aaba767
Mishiansari <163590796+Mishiansari@users.noreply.github.com>
2024-11-13 01:53:36 +0500

    hy
    this is my

--- src/components/Home/Skills/Skills.tsx
+++ src/components/Home/Skills/Skills.tsx
@@ -5,7 +5,7 @@ import SkillCard from "./SkillCard";
 export default function Skills  ()  {
   return (
     <div className="pt-16 pb:16 bg-[#635072]">
-      <h1 className="bg-blue-800 w-fit text-center px-4 py-3 mx-auto text-whitetext-2xl sm:text-3xl md:text-4xl uppercase -rotate-6">Skills</h1>
+      <h1 className="bg-blue-800 w-fit text-center px-4 py-3 mx-auto text-white text-2xl sm:text-3xl md:text-4xl uppercase -rotate-6">Skills</h1>
         <br></br><br></br><br></br>
         <div className="mt:20 w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3
         lg:grid-4 xl:grid-cols-5 gap-4 items-center">
